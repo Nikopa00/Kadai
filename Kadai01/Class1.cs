225a83d9f4f2a71390d434d15f8e529b5294727d
Nikopa00 <66275710+Nikopa00@users.noreply.github.com>
2020-07-15 03:02:49 +0900

    add: ファーストコミット

---
+++ Kadai01/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kadai01
+{
+    public class Class1
+    {
+        private int _textvalue;
+        private int _labelvalue;
+        private int sum;
+
+        public Class1(int Textvalue, int Labelvalue)
+        {
+            _textvalue = Textvalue;
+            _labelvalue = Labelvalue;
+
+            // 他に上手い方法があればそっちでやる
+            var a = new List<int>()
+            {
+                _textvalue, _labelvalue
+            };
+
+            sum = a.Sum();
+
+            // <読み取り専用にしてくれるらしい>：Todo
+            //List<int> ReadOnly(IReadOnlyCollection<int> entities)
+            //{
+            //    var a = new List<int>()
+            //     {
+            //        _textvalue, _labelvalue
+            //     };
+
+
+            //    sum = a.Sum();
+            //}
+        }
+
+        public int Get()
+        {
+            return sum;
+        }
+    }
+}
---
+++ Kadai01/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kadai01
+{
+    public partial class Form1 : Form
+    {
+        private int _num;
+        private int _num2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TashizanButton_Click(object sender, EventArgs e)
+        {
+            // TextBoxの値取得
+            /// Parse = 文字列を数字に変換
+            _num = int.Parse(InputTextBox1.Text);
+
+            // Labelの値取得
+            _num2 = int.Parse(ResultLabel1.Text);
+
+            // public Class1の初期値としてTextBoxとLabelの値を使用する
+            var Culum = new Class1(_num, _num2);
+
+            // Labelにreturnで返した合計値を格納する
+            // 本当はintでやりたかったがそれはおいおい
+            ResultLabel1.Text = Culum.Get().ToString();
+
+        }
+    }
+}
